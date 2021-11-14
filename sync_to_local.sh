e7640acff832dc85fc893b7bc46c78b573f3eb50
ctadel-office <prajwal@tradeindia.com>
2021-11-15 00:17:25 +0530

    Implemented symbolic links to for all the dot files

--- sync_to_local.sh
+++ sync_to_local.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 read -p "Syncronize linux_ files to local server?(yes/*): " confirm
 if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]];then
-    cp vimrc ~/.vimrc
-    cp bashpd ~/.bashpd
-    cp inputrc ~/.inputrc
+    ln -sf $LIN_/vimrc ~/.vimrc
+    ln -sf $LIN_/bashpd ~/.bashpd
+    ln -sf $LIN_/inputrc ~/.inputrc
+    ln -sf $LIN_/psqlrc ~/.psqlrc
     printf "${green}Sync'd linux_ files to local Server.${normal}\n"
 else
     printf "Operation canceled by user.."
