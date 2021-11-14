39874638b18022912da62dfcf4f3927cabe9d0ae
Prajwal Dev <prajwal@tradeindia.com>
2021-11-15 00:22:59 +0530

    minor change

--- sync_to_local.sh
+++ sync_to_local.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+export LIN_="/home/$USER/gitlib/linux_"
 read -p "Syncronize linux_ files to local server?(yes/*): " confirm
 if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]];then
     ln -sf $LIN_/vimrc ~/.vimrc
