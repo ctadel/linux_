e24a9efa0aded5d291bd748eee3aa8d2a028b904
ctadel-office <prajwal@tradeindia.com>
2021-12-03 09:57:48 +0530

    Added symlink to neovim config and fixed the LIN_ path

--- sync_to_local.sh
+++ sync_to_local.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
-export LIN_=$(pwd)
+export LIN_=$HOME/gitlib/linux_
 read -p "Syncronize linux_ files to local server?(yes/*): " confirm
 if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]];then
     ln -sf $LIN_/vimrc ~/.vimrc
     ln -sf $LIN_/bashpd ~/.bashpd
     ln -sf $LIN_/inputrc ~/.inputrc
     ln -sf $LIN_/psqlrc ~/.psqlrc
+    if [ -f  /usr/bin/nvim ]; then
+        if ! [ -d  ~/.config/nvim ]; then
+            mkdir -p ~/.config/nvim
+        fi
+        ln -sf $LIN_/vimrc ~/.config/nvim/init.vim
+    fi
     printf "${green}Sync'd linux_ files to local Server.${normal}\n"
 else
     printf "Operation canceled by user.."
