3445c2f338dc11897fb6a2fbe808c69dcda4c1b7
ctadel <nep.prajwal@gmail.com>
2024-05-11 01:50:20 +0530

    added exec script to automatically source bashpd

--- shell/add_source.sh
+++ shell/add_source.sh
@@ -1,2 +1,21 @@
-!#/bin/bash
+#!/bin/bash
 
+code_to_search='[[ ! -f ~/.bashpd ]] || source ~/.bashpd'
+
+file_paths=(
+    ~/.zshrc
+    ~/.bashrc
+)
+
+for file_path in "${file_paths[@]}"; do
+    if [ -f "$file_path" ]; then
+        if ! grep -qF "$code_to_search" "$file_path"; then
+            echo "$code_to_search" >> "$file_path"
+            echo "Code added to $file_path"
+        else
+            echo "Code already exists in $file_path"
+        fi
+    else
+        echo "File $file_path does not exist"
+    fi
+done
